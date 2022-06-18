9f72025919b1a5616ddb659fdc919038a0e27ef4
cwild10 <cwild@student.fullsail.edu>
2022-06-17 21:50:02 -0400

    test

---
+++ midterm practical.cpp
@@ -0,0 +1,39 @@
+// midterm practical.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <conion.h>
+using namespace std;
+ 
+int main()
+{
+    clrscr();
+    char ch1[10], ch2;
+
+    cout << " Random question";
+    cout << "A,B,C,D";
+    cin >>ch2;
+    if (ch2 == 'a')
+    {
+        cout << "correct";
+    }
+    else
+    {
+        cout << "Wrong";
+    }
+    clrscr();
+
+    cout << " Random question dsffs";
+    cout << "A,B,C,D";
+    cin >> ch2;
+    if (ch2 == 'a')
+    {
+        cout << "correct";
+    }
+    else
+    {
+        cout << "Wrong";
+    }
+    clrscr();
+
+}
